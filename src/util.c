4c8ff0c4577a6700198c2468e031692606cc0671
Ionel Pop <ionelalexandru.pop@gmail.com>
2024-10-16 14:53:01 +0200

    Update util.c & util.h
    Added a function to parse the correct path of a file

--- src/util.c
+++ src/util.c
@@ -1,12 +1,13 @@
 #include "raymath.h"
+#include <string.h>
 #include "stdio.h"
 
 #include "util.h"
 
 float lerpf(float a, float b, float scale, float dt) {
-  // raymath.h provides `EPSILON`
-  if (a > b - EPSILON && a < b + EPSILON) return b;
-  else return a + (b - a) * (1 - exp2f(-scale * dt));
+    // raymath.h provides `EPSILON`
+    if (a > b - EPSILON && a < b + EPSILON) return b;
+    else return a + (b - a) * (1 - exp2f(-scale * dt));
 }
 
 void printArt(char* file_path) {
@@ -20,4 +21,23 @@ void printArt(char* file_path) {
     }
 
     fclose(file_ptr);
+}
+
+void truncate_str_after_directory_separator(char *path) {
+    for (int i = strlen(path); i >= 0; --i) {
+        if (path[i] == '\\' || path[i] == '/') {
+            path[i+1] = '\0';
+            return;
+        }
+    }
+}
+
+const char* ASSETS_FOLDER = "assets";
+void parse_asset_path(char* buffer, char* local_path, const char* asset_name) {
+    // Parse the relative path of an asset file
+#ifdef _WIN32
+    sprintf(buffer, "%s%s\\%s", local_path, ASSETS_FOLDER, asset_name);
+#else
+    sprintf(buffer, "%s%s/%s", local_path, ASSETS_FOLDER, asset_name);
+#endif
 }
--- src/util.h
+++ src/util.h
@@ -10,5 +10,9 @@ float lerpf(float a, float b, float scale, float dt);
 
 void printArt(char* file_path);
 
+void truncate_str_after_directory_separator(char *path);
+
+void parse_asset_path(char* buffer, char* local_path, const char* asset_name);
+
 #endif // UTIL_H
 
